75b50c4249ced56d1a41ec62618687b4317e3ae4
Péter Szakszon <szp0530@gmail.com>
2022-03-03 21:07:45 +0100

    Add skeleton form creating missing thumbnail

--- space.go
+++ space.go
@@ -12,6 +12,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/hashicorp/go-hclog"
 )
@@ -26,6 +27,9 @@ type ServerConfig struct {
 type Server struct {
 	conf    *ServerConfig
 	handler http.Handler
+
+	thumbnailMutex    sync.Mutex
+	pendingThumbnails map[string][]chan error
 }
 
 func NewServer(conf ServerConfig) (*Server, error) {
@@ -34,12 +38,13 @@ func NewServer(conf ServerConfig) (*Server, error) {
 	}
 
 	s := &Server{
-		conf: &conf,
+		conf:              &conf,
+		pendingThumbnails: make(map[string][]chan error),
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/source/", s.sourceHandler())
-	//mux.Handle("/thumbnail/", s.thumbnailHandler())
+	mux.Handle("/thumbnail/", s.thumbnailHandler())
 
 	h := http.Handler(mux)
 	h = s.slashRemover(h)
@@ -51,14 +56,10 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.handler.ServeHTTP(w, r)
 }
 
-func (s *Server) sourceHandler() http.Handler {
+func (s *Server) thumbnailHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" || r.Method == "HEAD" {
-			s.serveObject(w, r)
-			return
-		}
-		if r.Method == "PUT" {
-			s.saveObject(w, r)
+			s.serveThumbnail(w, r)
 			return
 		}
 
@@ -67,12 +68,8 @@ func (s *Server) sourceHandler() http.Handler {
 	})
 }
 
-func removeSourcePrefix(url string) string {
-	return strings.Replace(url, "/source/", "", 1)
-}
-
-func (s *Server) serveObject(w http.ResponseWriter, r *http.Request) {
-	key := strings.TrimSpace(removeSourcePrefix(r.URL.Path))
+func (s *Server) serveThumbnail(w http.ResponseWriter, r *http.Request) {
+	key := strings.TrimSpace(removePrefix(r.URL.Path, "/thumbnail/"))
 	err := s.validateKey(key)
 	if err != nil {
 		s.conf.Logger.Error("Invalid key", "error", err)
@@ -80,16 +77,135 @@ func (s *Server) serveObject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	p := filepath.Join(s.conf.SourceDir, objPathRel(key))
-	s.conf.Logger.Debug("Open", "path", p)
-	f, err := os.Open(p)
+	f, err := s.openThumbnail(key)
+	if err != nil && !os.IsNotExist(err) {
+		s.conf.Logger.Error("Failed to open thumbnail", "key", key, "error", err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+	if os.IsNotExist(err) {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
 	if err != nil {
-		if os.IsNotExist(err) {
-			http.Error(w, "Not found", http.StatusNotFound)
+		s.conf.Logger.Error("Failed to get file info", "key", key, "error", err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+
+	//w.Header().Set("cache-control", "public, max-age=1209600") // 2 Weeks = 1209600 Seconds
+	//w.Header().Set("etag", "\""+md5Hash+"\"")
+
+	if r.Method == "HEAD" {
+		w.Header().Set("content-type", mime.TypeByExtension(filepath.Ext(fi.Name())))
+		w.Header().Set("content-length", strconv.FormatInt(fi.Size(), 10))
+		w.Header().Set("last-modified", fi.ModTime().UTC().Format(http.TimeFormat))
+		w.WriteHeader(200)
+		return
+	}
+
+	http.ServeContent(w, r, "", fi.ModTime(), f)
+	return
+}
+
+func (s *Server) thumbnailPath(key string) string {
+	return filepath.Join(s.conf.ThumbnailDir, keyFilepath(key))
+}
+
+func (s *Server) openThumbnail(key string) (*os.File, error) {
+	path := s.thumbnailPath(key)
+	s.conf.Logger.Debug("Open", "path", path)
+	f, err := os.Open(path)
+	if (err != nil && !os.IsNotExist(err)) || err == nil {
+		return f, err
+	}
+
+	s.thumbnailMutex.Lock()
+	ch := make(chan error, 1)
+	s.pendingThumbnails[key] = append(s.pendingThumbnails[key], ch)
+	if len(s.pendingThumbnails[key]) == 1 {
+		go s.createThumbnail(key, path)
+	}
+	s.thumbnailMutex.Unlock()
+
+	err = <-ch
+	if err != nil {
+		return nil, err
+	}
+	s.conf.Logger.Debug("Open", "path", path)
+	return os.Open(path)
+}
+
+func (s *Server) createThumbnail(key, path string) {
+	_, err := os.Stat(path)
+	if err != nil && !os.IsNotExist(err) {
+		s.sendThumbnailResult(key, err)
+		return
+	}
+	if err == nil {
+		s.sendThumbnailResult(key, nil)
+		return
+	}
+
+	err = fmt.Errorf("not implemented")
+	s.sendThumbnailResult(key, err)
+}
+
+func (s *Server) sendThumbnailResult(key string, err error) {
+	s.thumbnailMutex.Lock()
+	defer s.thumbnailMutex.Unlock()
+
+	for _, ch := range s.pendingThumbnails[key] {
+		if err != nil {
+			ch <- err
+		}
+		close(ch)
+	}
+	s.pendingThumbnails[key] = s.pendingThumbnails[key][:0]
+}
+
+func (s *Server) sourceHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" || r.Method == "HEAD" {
+			s.serveSource(w, r)
 			return
 		}
+		if r.Method == "PUT" {
+			s.saveSource(w, r)
+			return
+		}
+
+		http.Error(w, "Error", http.StatusBadRequest)
+		return
+	})
+}
+
+func removePrefix(url, prefix string) string {
+	return strings.Replace(url, prefix, "", 1)
+}
+
+func (s *Server) serveSource(w http.ResponseWriter, r *http.Request) {
+	key := strings.TrimSpace(removePrefix(r.URL.Path, "/source/"))
+	err := s.validateKey(key)
+	if err != nil {
+		s.conf.Logger.Error("Invalid key", "error", err)
+		http.Error(w, "Invalid key", http.StatusBadRequest)
+		return
+	}
+
+	p := filepath.Join(s.conf.SourceDir, keyFilepath(key))
+	s.conf.Logger.Debug("Open", "path", p)
+	f, err := os.Open(p)
+	if err != nil && !os.IsNotExist(err) {
 		s.conf.Logger.Error("Failed to open file", "path", p, "error", err)
 		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+	if os.IsNotExist(err) {
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
 	defer f.Close()
@@ -117,8 +233,8 @@ func (s *Server) serveObject(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func (s *Server) saveObject(w http.ResponseWriter, r *http.Request) {
-	key := strings.TrimSpace(removeSourcePrefix(r.URL.Path))
+func (s *Server) saveSource(w http.ResponseWriter, r *http.Request) {
+	key := strings.TrimSpace(removePrefix(r.URL.Path, "/source/"))
 	err := s.validateKey(key)
 	if err != nil {
 		s.conf.Logger.Error("Invalid key", "error", err)
@@ -126,7 +242,7 @@ func (s *Server) saveObject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	p := filepath.Join(s.conf.SourceDir, objPathRel(key))
+	p := filepath.Join(s.conf.SourceDir, keyFilepath(key))
 	dir := filepath.Dir(p)
 	err = os.MkdirAll(dir, 0754)
 	if err != nil {
@@ -146,14 +262,14 @@ func (s *Server) saveObject(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func objPathRel(key string) string {
+func keyFilepath(key string) string {
 	return filepath.FromSlash(key)
 }
 
-var objKeyRE *regexp.Regexp = regexp.MustCompile(`^[a-zA-Z0-9/._-]+$`)
+var keyRE *regexp.Regexp = regexp.MustCompile(`^[a-zA-Z0-9/._-]+$`)
 
 func (s *Server) validateKey(key string) error {
-	if !objKeyRE.Match([]byte(key)) {
+	if !keyRE.Match([]byte(key)) {
 		return fmt.Errorf("invalid key: %v", key)
 	}
 
